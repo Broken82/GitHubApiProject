ebe5aa15f7151af72317ee059e11368729861842
Broken82 <zgrass11@gmail.com>
2025-08-03 13:47:21 +0200

    Hide application.properties

--- src/main/java/com/broken/githubapiproject/config/RestClientConfig.java
+++ src/main/java/com/broken/githubapiproject/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.broken.githubapiproject.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,7 +8,8 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
-    private final static String TOKEN = "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";
+    @Value("${github.token}")
+    private String TOKEN;
 
     @Bean
     public RestClient restClient(){
